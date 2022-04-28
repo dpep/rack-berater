28b2869446c93db44266de6a297137550be1effc
Daniel Pepper <pepper.daniel@gmail.com>
2022-04-27 21:52:39 -0700

    test coverage

---
+++ spec/rails_spec.rb
@@ -0,0 +1,47 @@
+require 'rspec/rails'
+
+describe Rack::Berater do
+  before do
+    class OkController < ActionController::Base
+      around_action :limit
+
+      def limit(&block)
+        Berater(:key, 1) { yield }
+      end
+
+      def index
+        head :ok
+      end
+    end
+
+    Rails.application = Class.new(Rails::Application) do
+      config.eager_load = false
+      config.hosts.clear # disable hostname filtering
+      # config.logger = ActiveSupport::Logger.new($stdout)
+    end
+    Rails.application.middleware.use described_class
+    Rails.initialize!
+
+    Rails.application.routes.draw do
+      get '/' => 'ok#index'
+    end
+  end
+
+  let(:app) { Rails.application }
+
+  after do
+    Rails.application = nil
+  end
+
+  context 'when within limits' do
+    before { Berater.test_mode = :pass }
+
+    it { expect(get('/')).to be_ok }
+  end
+
+  context 'when past limits' do
+    before { Berater.test_mode = :fail }
+
+    it { expect(get('/').status).to be 429 }
+  end
+end
